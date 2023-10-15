688ec8c28c48f6fa5ca9b19e6aba1245591ccc5b
tanigutaic <tanigutaic@ecs.vuw.ac.nz>
2023-10-15 15:41:49 +0900

    Fix an error.Added clientonly and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import ClientOnly from './components/ClientOnly'
 import Navbar from './components/navbar/Navbar'
 import './globals.css'
 import { Nunito } from 'next/font/google'
@@ -19,7 +20,10 @@ export default function RootLayout({
   return(
     <html lang="en">
     <body>{children}
+    <ClientOnly>
     <Navbar />
+    </ClientOnly>
+    {children}
     </body>
     </html>
   )
