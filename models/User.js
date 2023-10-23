cf17520e193ea18f29bc957f58e46a319a889446
tanigutaic <tanigutaic@ecs.vuw.ac.nz>
2023-10-24 02:18:17 +0900

    Added debug for testing database

--- models/User.js
+++ models/User.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
 
+mongoose.set('debug', true);  
+
 const UserSchema = new mongoose.Schema({
     username: String,
     email: String, 
     password: String,
     resetToken: String,
     resetTokenExpiration: Date,
-    location: String,   //for recommendation
-    purchaseHistory: [String]
+    location: String,//for recommendation
+    purchaseHistory: [String] //for recommendation
 });
 
 const User = mongoose.model('User', UserSchema);
