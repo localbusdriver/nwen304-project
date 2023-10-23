d2a57f6f45b5a315c4c74cc086148a396bc64360
jelly <jamesla200210@gmail.com>
2023-10-23 18:20:26 +1300

    Troubleshooting

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ mongoose.connect(MONGO_URL, {
 
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
